5d043afaaf33a7c98c5aa12507e30ae3e8fb0e53
AgustinDiLeone <agustindileone@gmail.com>
2024-08-29 20:28:47 -0300

    ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,56 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FormsModule],
+  imports: [RouterOutlet,FormsModule,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'prog_IV';
+
+  edadUno: number | undefined; 
+  edadDos: number | undefined;
+
+  mostrarDatos() {
+
+    const miDiv = document.getElementById('resultado')!;
+    if(this.verificarDatos()){
+      const numEdadUno = Number(this.edadUno);
+      const numEdadDos = Number(this.edadDos);
+
+      const suma: number = numEdadUno + numEdadDos;
+      const promedio: number = suma / 2;
+
+      // Inserta los resultados en el div
+      miDiv.innerHTML = ` <p>
+                            <label for="suma">Suma:</label>
+                            <input type="text" id="suma" value="${suma}" readonly>
+                          </p>
+                          <p>
+                            <label for="promedio">Promedia:</label>
+                            <input type="text" id="promedio" value="${promedio}" readonly>
+                          </p>`;
+    }else{
+      miDiv.innerHTML = `<p>Ingrese edades validas</p>`;
+    }
+  }
+  verificarDatos() {
+    if(this.edadUno == undefined || this.edadDos == undefined){
+      return false;
+    }else{
+      return true;
+    }
+  }
+  limpiarDatos() {
+    const miDiv = document.getElementById('resultado')!;
+    miDiv.innerHTML = "";
+
+    this.edadUno = undefined; 
+    this.edadDos = undefined;
+  }
+
 }
